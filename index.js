0d9daaae143083c9ff034fa9af6c560d1fab4b78
tokyoparis <115654236+tokyoparis@users.noreply.github.com>
2022-12-03 20:22:49 -0500

    elements, append child created JS

---
+++ index.js
@@ -0,0 +1,28 @@
+// elements
+
+const container = document.createElement("div");
+const header = document.createElement("h1");
+const outPut = document.createElement("p");
+
+
+
+
+// append child
+
+document.body.appendChild(container);
+container.appendChild(header);
+container.appendChild(outPut);
+
+
+
+
+
+//
+header.innerHTML = "Digital Clock"
+outPut.innerHTML = "Here";
+
+
+
+
+
+
